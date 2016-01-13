bfe2fb7f73f347fe283abc6227f6bc40f6a752c0
elc1798 <subliminalmau5@gmail.com>
2016-01-13 17:44:27 -0500

    Remove merge conflict

--- src/modules/VisionModule1.java
+++ src/modules/VisionModule1.java
@@ -1,6 +1,7 @@
 package modules;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
@@ -13,9 +14,9 @@ import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
-import gui.Main;
 import gui.DoubleSliderVariable;
 import gui.IntegerSliderVariable;
+import gui.Main;
 import vision.VisionModule;
 
 public class VisionModule1 extends VisionModule {
@@ -26,6 +27,8 @@ public class VisionModule1 extends VisionModule {
     public IntegerSliderVariable minV = new IntegerSliderVariable("Min V", 53,  0,  255);
     public IntegerSliderVariable maxV = new IntegerSliderVariable("Max V", 255, 0, 255);
     public DoubleSliderVariable AREA_THRESHOLD = new DoubleSliderVariable("AREA THRESH", 45.0, 0.0, 500.0);
+    public boolean anglePrinted = false;
+
     public void run(Main app, Mat frame) {
         app.postImage(frame, "Camera", this);
         Mat hsv = new Mat();
@@ -42,18 +45,21 @@ public class VisionModule1 extends VisionModule {
         Core.bitwise_and(channels.get(0), channels.get(1), channels.get(3));
         Core.bitwise_and(channels.get(2), channels.get(3), channels.get(3));
         Mat erodeKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
-        Mat dilateKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
+        Mat dilateKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5, 5));
         Imgproc.erode(channels.get(3), channels.get(3), erodeKernel);
         Imgproc.dilate(channels.get(3), channels.get(3), dilateKernel);
-        
+        app.postImage(channels.get(3), "After erode/dilate", this);
+
         // Locate the goals
         Mat drawn = frame.clone();
         Mat edges = new Mat();
         ArrayList<MatOfPoint> contour = new ArrayList<MatOfPoint>();
-        Imgproc.Canny(channels.get(3), edges, 0, 100);
-        Imgproc.findContours(edges, contour, new Mat() , Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.findContours(channels.get(3), contour, new Mat() , Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+        double largestArea = 0.0;
+        RotatedRect largestRect = new RotatedRect();
         for (int i = 0; i < contour.size(); i++) {
-            if (Imgproc.contourArea(contour.get(i)) < AREA_THRESHOLD.value()) {
+            System.out.println(Imgproc.contourArea(contour.get(i)));
+        	if (Imgproc.contourArea(contour.get(i)) < AREA_THRESHOLD.value()) {
                 continue;
             }
             MatOfPoint2f tmp = new MatOfPoint2f();
