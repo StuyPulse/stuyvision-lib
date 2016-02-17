38c75efdb4dcc896cf414f17d172a4a7ae99ca70
Wilson Berkow <wilsonberkow@gmail.com>
2016-02-17 16:47:09 -0500

    Adjust thresholds and remove use of gray-filtering (informed by hands-on tests, and using the new camera configuration method)

--- src/modules/StuyVisionModule.java
+++ src/modules/StuyVisionModule.java
@@ -22,24 +22,23 @@ import gui.Main;
 
 import java.util.Arrays;
 import java.io.PrintStream;
-import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import gui.DoubleSliderVariable;
 import gui.IntegerSliderVariable;
 
 public class StuyVisionModule extends VisionModule {
-    public IntegerSliderVariable minH_GREEN = new IntegerSliderVariable("Min H Green", 0,  0,  255);
-    public IntegerSliderVariable maxH_GREEN = new IntegerSliderVariable("Max H Green", 94,  0,  255);
-    public IntegerSliderVariable minS_GREEN = new IntegerSliderVariable("Min S Green", 88, 0, 255);
+    public IntegerSliderVariable minH_GREEN = new IntegerSliderVariable("Min H Green", 58,  0,  255);
+    public IntegerSliderVariable maxH_GREEN = new IntegerSliderVariable("Max H Green", 123,  0,  255);
+    public IntegerSliderVariable minS_GREEN = new IntegerSliderVariable("Min S Green", 104, 0, 255);
     public IntegerSliderVariable maxS_GREEN = new IntegerSliderVariable("Max S Green", 255,  0,  255);
-    public IntegerSliderVariable minV_GREEN = new IntegerSliderVariable("Min V Green", 200,  0,  255);
-    public IntegerSliderVariable maxV_GREEN = new IntegerSliderVariable("Max V Green", 255, 0, 255);
-    public IntegerSliderVariable minH_GRAY = new IntegerSliderVariable("Min H Gray", 0,  0,  255);
-    public IntegerSliderVariable maxH_GRAY = new IntegerSliderVariable("Max H Gray", 255,  0,  255);
-    public IntegerSliderVariable minS_GRAY = new IntegerSliderVariable("Min S Gray", 17, 0, 255);
-    public IntegerSliderVariable maxS_GRAY = new IntegerSliderVariable("Max S Gray", 118,  0,  255);
-    public IntegerSliderVariable minV_GRAY = new IntegerSliderVariable("Min V Gray", 120,  0,  255);
+    public IntegerSliderVariable minV_GREEN = new IntegerSliderVariable("Min V Green", 20,  0,  255);
+    public IntegerSliderVariable maxV_GREEN = new IntegerSliderVariable("Max V Green", 155, 0, 255);
+    public IntegerSliderVariable minH_GRAY = new IntegerSliderVariable("Min H Gray", 60,  0,  255);
+    public IntegerSliderVariable maxH_GRAY = new IntegerSliderVariable("Max H Gray", 197,  0,  255);
+    public IntegerSliderVariable minS_GRAY = new IntegerSliderVariable("Min S Gray", 76, 0, 255);
+    public IntegerSliderVariable maxS_GRAY = new IntegerSliderVariable("Max S Gray", 255,  0,  255);
+    public IntegerSliderVariable minV_GRAY = new IntegerSliderVariable("Min V Gray", 0,  0,  255);
     public IntegerSliderVariable maxV_GRAY = new IntegerSliderVariable("Max V Gray", 244, 0, 255);
     public int threshBlockSizeH = 78;
     public int threshConstantH = 0;
@@ -50,10 +49,13 @@ public class StuyVisionModule extends VisionModule {
     public int threshBlockSizeV = 76;
     public int threshConstantV = 0;
     public boolean useV = true;
-    public double AREA_THRESHOLD = 600.0;
-    public DoubleSliderVariable areaThreshold = new DoubleSliderVariable("Area Threshold", 200.0, 0.0, 700.0);
-    public double r1 = 1.25;
-    public double r2 = 3.00;
+    private static double frameWidth = 640.0;
+    private static double frameHeight = 480.0;
+    private static double frameArea = frameWidth * frameHeight;
+    public DoubleSliderVariable minAreaThreshold = new DoubleSliderVariable("Min Area Threshold", 200.0, 0.0, 700.0);
+    public DoubleSliderVariable maxAreaThreshold = new DoubleSliderVariable("Max Area Threshold", frameArea * 0.1, 0.0, frameArea);
+    public DoubleSliderVariable r1 = new DoubleSliderVariable("Min ratio theshold", 1.1, 0.0, 4.0);
+    public DoubleSliderVariable r2 = new DoubleSliderVariable("Max ratio theshold", 3.0, 0.0, 4.0);
 
     static {
         String dir = StuyVisionModule.class.getClassLoader().getResource("").getPath();
@@ -177,7 +179,7 @@ public class StuyVisionModule extends VisionModule {
 
     private boolean aspectRatioThreshold(double height, double width) {
         double ratio = width / height;
-        return (r1 < ratio && ratio < r2) || (1 / r2< ratio && ratio < 1 / r1);
+        return (r1.value() < ratio && ratio < r2.value()) || (1 / r2.value() < ratio && ratio < 1 / r1.value());
     }
 
     private double[] getLargestGoal(Mat frame, Mat filteredImage, Main app) {
@@ -194,7 +196,7 @@ public class StuyVisionModule extends VisionModule {
 
         for (int i = 0; i < contour.size(); i++) {
             double currArea = Imgproc.contourArea(contour.get(i));
-            if (currArea < areaThreshold.value()) {
+            if (currArea < minAreaThreshold.value() || currArea > maxAreaThreshold.value()) {
                 continue;
             }
             MatOfPoint2f tmp = new MatOfPoint2f();
@@ -249,7 +251,6 @@ public class StuyVisionModule extends VisionModule {
         Mat hsv = new Mat();
         Imgproc.cvtColor(frame, hsv, Imgproc.COLOR_BGR2HSV);
         ArrayList<Mat> greenFilterChannels = new ArrayList<Mat>();
-        ArrayList<Mat> grayFilterChannels = new ArrayList<Mat>();
 
         // Split HSV channels and process each channel
         Core.split(hsv, greenFilterChannels);
@@ -259,14 +260,6 @@ public class StuyVisionModule extends VisionModule {
                 greenFilterChannels.get(1));
         Core.inRange(greenFilterChannels.get(2), new Scalar(minV_GREEN.value()), new Scalar(maxV_GREEN.value()),
                 greenFilterChannels.get(2));
-
-        Core.split(hsv, grayFilterChannels);
-        Core.inRange(grayFilterChannels.get(0), new Scalar(minH_GRAY.value()), new Scalar(maxH_GRAY.value()),
-                grayFilterChannels.get(0));
-        Core.inRange(grayFilterChannels.get(1), new Scalar(minS_GRAY.value()), new Scalar(maxS_GRAY.value()),
-                grayFilterChannels.get(1));
-        Core.inRange(grayFilterChannels.get(2), new Scalar(minV_GRAY.value()), new Scalar(maxV_GRAY.value()),
-                grayFilterChannels.get(2));
 
         // Merge channels and erode dilate to remove noise
         Mat erodeKernel = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3, 3));
@@ -278,21 +271,8 @@ public class StuyVisionModule extends VisionModule {
         Imgproc.erode(greenFiltered, greenFiltered, erodeKernel);
         Imgproc.dilate(greenFiltered, greenFiltered, dilateKernel);
 
-        Mat grayFiltered = new Mat();
-        Core.bitwise_and(grayFilterChannels.get(0), grayFilterChannels.get(1), grayFiltered);
-        Core.bitwise_and(grayFilterChannels.get(2), grayFiltered, grayFiltered);
-        Imgproc.erode(grayFiltered, grayFiltered, erodeKernel);
-        Imgproc.dilate(grayFiltered, grayFiltered, dilateKernel);
-
         if (withGui) {
             app.postImage(greenFiltered, "Green - After erode/dilate", this);
-            app.postImage(grayFiltered, "Gray - After erode/dilate", this);
-        }
-
-        // Merge "grayed" reflexite with green reflexite
-        Core.bitwise_or(greenFiltered, grayFiltered, greenFiltered);
-
-        if (withGui) {
             app.postImage(greenFiltered, "Merged", this);
         }
 
