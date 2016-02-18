706c4d578dad7ed6dd52832df4b08ecee68ed3c0
James Wang <jameswang9909@hotmail.com>
2016-02-18 21:05:56 +0000

    Add qt5-default and qt5-qmake to install scripts

--- install-opencv-tegra.sh
+++ install-opencv-tegra.sh
@@ -5,7 +5,7 @@ OPENCV_VERSION=3.0.0
 echo "Installing opencv-$OPENCV_VERSION for Nvidia Jetson TK1 on Tegra Processor ...\n"
 echo "================================================================================"
 sudo apt-get -y install libopencv-dev build-essential cmake git libgtk2.0-dev pkg-config python-dev python-numpy libdc1394-22 libdc1394-22-dev libjpeg-dev libpng12-dev libtiff4-dev libjasper-dev libavcodec-dev libavformat-dev libswscale-dev libxine-dev libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev libv4l-dev libtbb-dev libqt4-dev libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev x264 v4l-utils unzip
-sudo apt-get install libavformat-dev libavutil-dev libswscale-dev make cmake-curses-gui g++ libv4l-dev libeigen3-dev libglew1.6-dev libgtk2.0-dev
+sudo apt-get -y install libavformat-dev libavutil-dev libswscale-dev make cmake-curses-gui g++ libv4l-dev libeigen3-dev libglew1.6-dev libgtk2.0-dev qt5-qmake qt5-default
 
 cd lib
 echo "Downloading opencv-$OPENCV_VERSION"
