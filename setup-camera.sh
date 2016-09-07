3819f4c7772dc1cbb68f69dd69543d0ec02a3f05
Wilson Berkow <wilsonberkow@gmail.com>
2016-09-07 11:56:44 -0400

    Enable passing of -Dcamera=N option to specify video device to configure

--- setup-camera.sh
+++ setup-camera.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-v4l2-ctl -c exposure_auto=1,exposure_absolute=5,brightness=30,contrast=10,saturation=200,white_balance_temperature_auto=0,sharpness=50
-v4l2-ctl -c white_balance_temperature=4624
+# Get command line argument(s):
+device=0
+while getopts d:i option; do
+    case $option in
+        d) device=$OPTARG;;
+        i)
+            echo "Enter a video device to configure, or a number for /dev/videoN, of the following:"
+            ls /dev/video*
+            echo "Device:"
+            read device;;
+        \?) exit 1;;
+    esac
+done
+
+v4l2-ctl -d $device -c exposure_auto=1,exposure_absolute=5,brightness=30,contrast=10,saturation=200,white_balance_temperature_auto=0,sharpness=50
+v4l2-ctl -d $device -c white_balance_temperature=4624
